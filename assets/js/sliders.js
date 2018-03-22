96724954a50fd0c5767a42e27cd312335395bdc4
wdzajicek <tig.welder4@gmail.com>
2018-03-22 15:27:23 -0500

    Create custom JS for Baseball pages carousel

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -1,4 +1,6 @@
 // DO NOT PLACE IN A DOCUMENT READY FUNTION - it will break
+
+// Main Athletics carousel
 $('.heroSlider').slick({
   dots: true,
   slidesToShow: 1,
@@ -7,4 +9,14 @@ $('.heroSlider').slick({
   autoplaySpeed: 3000,
   prevArrow:"<img class='a-left control-c prev slick-prev' src='assets/img/dbl-prev.svg'>",
   nextArrow:"<img class='a-right control-c next slick-next' src='assets/img/dbl-next.svg'>"
+});
+// Baseball carousel
+$('.baseballSlider').slick({
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  prevArrow:"<img class='a-left control-c prev slick-prev' src='../../assets/img/dbl-prev.svg'>",
+  nextArrow:"<img class='a-right control-c next slick-next' src='../../assets/img/dbl-next.svg'>"
 });
