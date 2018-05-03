c14705cae4fff7da28b1ec85757fbccb6cbfbb75
wdzajicek <tig.welder4@gmail.com>
2018-05-03 15:50:23 -0500

    Add lazyLoad: ondemand; to slider.js to lazy load slides
    Add empty alt="" to slider's next and previous buttons for...
    accesibility issue.

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -2,21 +2,23 @@
 
 // Main Athletics carousel
 $('.heroSlider').slick({
+  lazyLoad: 'ondemand',
   dots: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 3000,
-  prevArrow:'<img class="a-left control-c prev slick-prev" src="assets/img/dbl-prev.svg">',
-  nextArrow:'<img class="a-right control-c next slick-next" src="assets/img/dbl-next.svg">'
+  prevArrow:'<img alt="" class="a-left control-c prev slick-prev" src="assets/img/dbl-prev.svg">',
+  nextArrow:'<img alt="" class="a-right control-c next slick-next" src="assets/img/dbl-next.svg">'
 });
 // Baseball carousel
 $('.baseballSlider').slick({
+  lazyLoad: 'ondemand',
   dots: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 3000,
-  prevArrow:'<img class="a-left control-c prev slick-prev" src="../../assets/img/dbl-prev.svg">',
-  nextArrow:'<img class="a-right control-c next slick-next" src="../../assets/img/dbl-next.svg">'
+  prevArrow:'<img alt="" class="a-left control-c prev slick-prev" src="../../assets/img/dbl-prev.svg">',
+  nextArrow:'<img alt="" class="a-right control-c next slick-next" src="../../assets/img/dbl-next.svg">'
 });
